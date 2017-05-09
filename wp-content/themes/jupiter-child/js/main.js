2f7a4f7abf220ccc3f786d593f9be6f7aacc431b
Moe Quraishi <mquraishi@causeforce.com>
2017-05-09 01:19:25 -0700

    style updates accordion for safety page

--- wp-content/themes/jupiter-child/js/main.js
+++ wp-content/themes/jupiter-child/js/main.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
     
-    $('.safety-accordion-section').addClass('vc_active');
-      function shortString(string) {
+
+    
+    var windowSize = $(window).width();
+    if (windowSize < 782) {
+        $('.safety-accordion-section').removeClass('vc_active');
+    } else {
+        $('.safety-accordion-section').addClass('vc_active');
+    }
+    
+    function shortString(string) {
             return '#menu-'+string+'_footer_menu>li.menu-item.menu-item-type-post_type.menu-item-object-page.menu-item-has-children>.sub-menu';
         }
     function shortStringOther(string) {
             return '#menu-'+string+'_footer_menu>li.menu-item.menu-item-type-post_type.menu-item-object-page.menu-item-has-children';
         }
-
+    
+// === Footer Resize Area === //
     $(window).resize(function() {
 
       if ($(this).width() < 782) {
@@ -19,13 +28,16 @@ $(document).ready(function() {
             $('.safety-accordion-section').addClass('vc_active');
         }
     });
+// === END Footer Resize === //
     
-    $(window).scroll(function() {
-       if ($(this).scrollTop() > 147) {
-           $('ul-class>').addClass('fixed-postion');
-       } else {
-           $('ul-class').removeClass('fixed-position');
-       }
-    });
-    
+// Scroll and become Fixed //
+//
+//    $(window).scroll(function() {
+//       if ($(this).scrollTop() > 147) {
+//           $('ul-class>').addClass('fixed-postion');
+//       } else {
+//           $('ul-class').removeClass('fixed-position');
+//       }
+//    });
+//    
 });
