c6d1c3545718d876597a993c98f85d251aa74337
Christian Solis <csolis@causeforce.com>
2017-05-11 15:09:19 -0700

    js for tabs

--- wp-content/themes/jupiter-child/js/main.js
+++ wp-content/themes/jupiter-child/js/main.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
     var windowSize = $(window).width();
     if (windowSize > 782) {
         $('.safety-accordion-section, .outfitters-accordion-section').addClass('vc_active');
+        
     } else {
         //click function for accordion toggle only if window is below 782px
         $('.vc_tta-panel-heading').click(function(e) {
@@ -12,7 +13,11 @@ $(document).ready(function() {
             $(this).next('.vc_tta-panel-body').slideToggle('slow');
             $('.wpb-js-composer .vc_tta .vc_tta-controls-icon.vc_tta-controls-icon-plus').toggleClass('show-minus').siblings().addClass('show-plus');
 //            $('.wpb-js-composer .vc_tta .vc_tta-controls-icon.vc_tta-controls-icon-plus').toggleClass('show');
+
         });
+
+        $('.mobile-nav-tab-container').append(jQuery('#mk-tabs-tabs-4'));
+
     }
     
     function shortString(string) {
@@ -26,9 +31,13 @@ $(document).ready(function() {
     $(window).resize(function() {
 
       if ($(this).width() < 782) {
+            $('.mobile-nav-tab-container').append(jQuery('#mk-tabs-tabs-4'));
+          
             $(shortStringOther('about, rider-hub, impact')).removeClass("toggle-active");
             $('.safety-accordion-section, .outfitters-accordion-section').removeClass('vc_active');
       } else {
+            $('.tab-content-container').prepend(jQuery('#mk-tabs-tabs-4'));
+          
             $(shortString('about, rider-hub, impact')).show();
             $(shortStringOther('about, rider-hub, impact')).addClass("toggle-active");
             $('.safety-accordion-section, .outfitters-accordion-section').addClass('vc_active');
