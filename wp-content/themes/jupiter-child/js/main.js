f16b064b153750b4d34dd0ee890aae5ecc31cf34
Christian Solis <csolis@causeforce.com>
2017-05-11 14:38:18 -0700

    copy of current

--- wp-content/themes/jupiter-child/js/main.js
+++ wp-content/themes/jupiter-child/js/main.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
     
-
+    $('.outfitters-accordion-section').removeClass('vc_active');
     
     var windowSize = $(window).width();
-    if (windowSize < 782) {
-        
-        $('.mobile-nav-tab-container').append(jQuery('#mk-tabs-tabs-4'));
-        
-        $('.safety-accordion-section').removeClass('vc_active');
+    if (windowSize > 782) {
+        $('.safety-accordion-section, .outfitters-accordion-section').addClass('vc_active');
     } else {
-        $('.safety-accordion-section').addClass('vc_active');
+        //click function for accordion toggle only if window is below 782px
+        $('.vc_tta-panel-heading').click(function(e) {
+            e.preventDefault();   
+            $(this).next('.vc_tta-panel-body').slideToggle('slow');
+            $('.wpb-js-composer .vc_tta .vc_tta-controls-icon.vc_tta-controls-icon-plus').toggleClass('show-minus').siblings().addClass('show-plus');
+//            $('.wpb-js-composer .vc_tta .vc_tta-controls-icon.vc_tta-controls-icon-plus').toggleClass('show');
+        });
     }
     
     function shortString(string) {
@@ -22,26 +25,18 @@ $(document).ready(function() {
 // === Footer Resize Area === //
     $(window).resize(function() {
 
-
-
       if ($(this).width() < 782) {
-
-            $('.mobile-nav-tab-container').append(jQuery('#mk-tabs-tabs-4'));
-
             $(shortStringOther('about, rider-hub, impact')).removeClass("toggle-active");
-            $('.safety-accordion-section').removeClass('vc_active');
             $('.safety-accordion-section, .outfitters-accordion-section').removeClass('vc_active');
       } else {
             $(shortString('about, rider-hub, impact')).show();
             $(shortStringOther('about, rider-hub, impact')).addClass("toggle-active");
-<<<<<<< HEAD
-            $('.safety-accordion-section').addClass('vc_active');
-
-            $('.tab-content-container').prepend(jQuery('#mk-tabs-tabs-4'));
-=======
             $('.safety-accordion-section, .outfitters-accordion-section').addClass('vc_active');
         }
     });
+    
+    
+    
 // === END Footer Resize === //
     
 // Scroll and become Fixed //
@@ -53,7 +48,5 @@ $(document).ready(function() {
 //           $('ul-class').removeClass('fixed-position');
 //       }
 //    });
-//    
-
-
+  
 });
